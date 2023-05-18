d54b894fdd2b0b37cc8253ad464a8b9d5fd91b0f
Dun Yan <ong.dy2004@gmail.com>
2023-05-18 12:08:01 +0800

    /softwares

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var softwaresRouter = require('./routes/softwaresRouter');
+var softwaresRouter = require('./routes/softwares');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/telehealth', softwaresRouter);
+app.use('/softwares', softwaresRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
