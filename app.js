7ab589bbffd626db6261f7c291df1eb035a2dd1c
Dun Yan <ong.dy2004@gmail.com>
2023-05-23 12:49:07 +0800

    clickable cards

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const lib = require('./public/contents/library');
+const lib = require('./private/library');
 
 var indexRouter = require('./routes/index');
 var softwaresRouter = require('./routes/softwares');
